583a177378b988506971575713bc6095a75f2ed5
Alexander Reitzel <funtimecoding@gmail.com>
2015-04-13 09:19:49 +0200

    improve metrics script to provide --ci-mode and run sonar

--- run-metrics.sh
+++ run-metrics.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-vendor/bin/phploc --count-tests src test $@
+#!/bin/sh -e
+if [ "${1}" = "--ci-mode" ]; then
+    shift
+    mkdir -p "build/log"
+    vendor/bin/phploc --count-tests src test | tee "build/log/phploc.log"
+    sonar-runner | tee "build/log/sonar-runner.log"
+else
+    vendor/bin/phploc --count-tests src test
+fi
