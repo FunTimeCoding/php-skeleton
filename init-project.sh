6486f6c0b615f84e89509fc472b9b909b522ac35
Alexander Reitzel <funtimecoding@gmail.com>
2016-01-22 09:38:45 +0100

    invert order of operating system check

--- init-project.sh
+++ init-project.sh
@@ -12,12 +12,12 @@ fi
 
 OPERATING_SYSTEM=$(uname)
 
-if [ "${OPERATING_SYSTEM}" = "Darwin" ]; then
-    SED="gsed"
-    FIND="gfind"
-else
+if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
     SED="sed"
     FIND="find"
+else
+    SED="gsed"
+    FIND="gfind"
 fi
 
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
