d18b78680382a85386e9d23fb3b0bdb6b13245a7
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-06 15:36:53 +0100

    unify init and sync scripts with other skeleton projects

--- init-project.sh
+++ init-project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-# This tool can be used to initialise the template after making a fresh copy to get started quickly.
-# The goal is to make it as easy as possible to create scripts that allow easy testing and continuous integration.
+# This tool is to initialize the project after cloning.
+# The goal is to make easy to create and test new projects.
 
 CAMEL=$(echo "${1}" | grep -E '^([A-Z][a-z0-9]+){2,}$') || CAMEL=""
 
@@ -22,13 +22,11 @@ fi
 
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
-echo "DASH: ${DASH}"
-echo "INITIALS: ${INITIALS}"
+rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|vendor|\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/PhpSkeleton/${2}/g" -e "s/php-skeleton/${3}/g" -e "s/bin\/ps/bin\/${4}/g" ${5}' '_' "${SED}" "${CAMEL}" "${DASH}" "${INITIALS}" '{}' \;
-git mv src/PhpSkeleton.php "src/${CAMEL}.php"
-git mv test/Unit/PhpSkeletonTest.php "test/Unit/${CAMEL}Test.php"
-git mv bin/ps "bin/${INITIALS}"
+git mv src/PhpSkeleton.php src/"${CAMEL}".php
+git mv test/Unit/PhpSkeletonTest.php test/Unit/"${CAMEL}"Test.php
+git mv bin/ps bin/"${INITIALS}"
 composer dump-autoload
-rm init-project.sh
-echo "Done. Files were edited and moved using git. Review those changes."
+echo "Done. Files were edited and moved. Review those changes."
--- sync-project.sh
+++ sync-project.sh
@@ -1,22 +1,35 @@
 #!/bin/sh -e
 
-usage()
-{
-    echo "Usage: ${0} TARGET_PROJECT"
-}
+TARGET_PROJECT="${1}"
 
-if [ "${1}" = "" ]; then
-    usage
+if [ "${TARGET_PROJECT}" = "" ]; then
+    echo "Usage: ${0} TARGET_PROJECT"
 
     exit 1
 fi
-
-TARGET_PROJECT="${1}"
 
 if [ ! -d "${TARGET_PROJECT}" ]; then
     echo "Target directory does not exist."
 
     exit 1
+fi
+
+CAMEL=$(head -n1 "${TARGET_PROJECT}"/README.md | awk '{ print $2 }' | grep -E '^([A-Z][a-z0-9]+){2,}$') || CAMEL=""
+
+if [ "${CAMEL}" = "" ]; then
+    echo "Could not determine project name."
+
+    exit 1
+fi
+
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
+    FIND="find"
+    SED="sed"
+else
+    FIND="gfind"
+    SED="gsed"
 fi
 
 cp ./*.md "${TARGET_PROJECT}"
@@ -28,6 +41,10 @@ cp .gitignore "${TARGET_PROJECT}"
 cp .php_cs "${TARGET_PROJECT}"
 cp .phpmd.xml "${TARGET_PROJECT}"
 cp .phpunit.ci.xml "${TARGET_PROJECT}"
-rm "${TARGET_PROJECT}/init-project.sh"
-rm "${TARGET_PROJECT}/sync-project.sh"
-echo "Done. Files were copied to ${TARGET_PROJECT}. Review those changes."
+cd "${TARGET_PROJECT}" || exit 1
+DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
+rm init-project.sh sync-project.sh
+# shellcheck disable=SC2016
+${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|vendor|\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/PhpSkeleton/${2}/g" -e "s/php-skeleton/${3}/g" -e "s/bin\/ps/bin\/${4}/g" ${5}' '_' "${SED}" "${CAMEL}" "${DASH}" "${INITIALS}" '{}' \;
+echo "Done. Files were copied to ${TARGET_PROJECT} and modified. Review those changes."
