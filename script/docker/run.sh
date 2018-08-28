0646975787eb1c1f3d6c52b4e6b32e5a9c470887
Alexander Reitzel <funtimecoding@gmail.com>
2018-08-28 16:55:14 +0200

    synchronize with shell-skeleton

--- script/docker/run.sh
+++ script/docker/run.sh
@@ -11,14 +11,16 @@ fi
 docker ps --all | grep --quiet php-skeleton && FOUND=true || FOUND=false
 
 if [ "${FOUND}" = false ]; then
+    WORKING_DIRECTORY=$(pwd)
+
     if [ "${DEVELOPMENT}" = true ]; then
-        docker create --name php-skeleton --volume $(pwd):/php-skeleton funtimecoding/php-skeleton
+        docker create --name php-skeleton --volume "${WORKING_DIRECTORY}:/php-skeleton" funtimecoding/php-skeleton
     else
         docker create --name php-skeleton funtimecoding/php-skeleton
     fi
 
     # TODO: Specifying the entry point overrides CMD in Dockerfile. Is this useful, or should all sub commands go through one entry point script? I'm inclined to say one entry point script per project.
-    #docker create --name php-skeleton --volume $(pwd):/php-skeleton --entrypoint /php-skeleton/bin/other.sh funtimecoding/php-skeleton
+    #docker create --name php-skeleton --volume "${WORKING_DIRECTORY}:/php-skeleton" --entrypoint /php-skeleton/bin/other.sh funtimecoding/php-skeleton
     #docker create --name php-skeleton funtimecoding/php-skeleton /php-skeleton/bin/other.sh
     # TODO: Run tests this way?
     #docker create --name php-skeleton funtimecoding/php-skeleton /php-skeleton/script/docker/test.sh
