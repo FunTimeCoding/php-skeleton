2b9f4692b7185fe93fd0c66459991b30cd9877a7
Alexander Reitzel <funtimecoding@gmail.com>
2019-05-07 15:17:08 +0200

    synchronize with skeleton

--- script/vagrant/create.bat
+++ script/vagrant/create.bat
@@ -1,4 +1,6 @@
 @echo off
+if not exist .venv python -m venv .venv
+
 if not exist tmp mkdir tmp
 
 if not exist tmp\bootstrap-salt.sh powershell -Command "Invoke-WebRequest https://bootstrap.saltstack.com -OutFile tmp/bootstrap-salt.sh"
