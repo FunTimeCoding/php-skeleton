4539e5b855ee0dc00dae5bcd7c0a8bf375bfdfa1
Alexander Reitzel <funtimecoding@gmail.com>
2019-12-06 18:13:28 +0100

    add inventory, update nginx configuration for buster - maybe it will work the same on stretch

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -30,12 +30,20 @@ elif [ "${CODENAME}" = stretch ]; then
     cp /vagrant/configuration/site.txt /etc/nginx/sites-available/default
     systemctl restart nginx
 
-    # Let vagrant user read web server logs.
-    usermod --append --groups adm vagrant
-
-    # Download Composer manually because the Debian package depends on PHP 7.0.
-    wget --no-verbose --output-document /usr/local/bin/composer https://getcomposer.org/download/1.9.0/composer.phar
+    wget --no-verbose --output-document /usr/local/bin/composer https://getcomposer.org/download/1.9.1/composer.phar
     chmod +x /usr/local/bin/composer
 elif [ "${CODENAME}" = buster ]; then
     apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ronn dos2unix ansible
+
+    apt-get --quiet 2 install php-cli php-fpm php-xdebug php-xml php-mbstring php-zip php-ast composer
+    cp /vagrant/configuration/xdebug.ini /etc/php/7.3/mods-available/xdebug.ini
+    systemctl restart php7.3-fpm
+
+    apt-get --quiet 2 install nginx-light curl
+
+    cp /vagrant/configuration/site.txt /etc/nginx/sites-available/default
+    systemctl restart nginx
 fi
+
+# Let vagrant user read web server logs.
+usermod --append --groups adm vagrant
