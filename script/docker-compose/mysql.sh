c337314749b01d5eeb12e792f4d068dce8a23c39
Alexander Reitzel <funtimecoding@gmail.com>
2020-01-20 16:20:09 +0100

    allow TCP connections to mysql

--- script/docker-compose/mysql.sh
+++ script/docker-compose/mysql.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -e
 
-docker exec -it php-skeleton_mysql-service_1 mysql --user=root --password=root php_skeleton
+if [ "${1}" = '--tcp' ]; then
+    mysql --user=root --password=root --host localhost --port 3307 --protocol TCP php_skeleton
+else
+    docker exec -it php-skeleton_mysql-service_1 mysql --user=root --password=root php_skeleton
+fi
