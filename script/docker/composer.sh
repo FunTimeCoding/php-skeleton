6bd6a717d092761707ae2d1e1e594ad360e8f751
Alexander Reitzel <funtimecoding@gmail.com>
2021-10-26 21:33:35 +0200

    extend editorconfig, use linebreaks for docker command

--- script/docker/composer.sh
+++ script/docker/composer.sh
@@ -2,4 +2,10 @@
 
 # TODO: This rather needs the Dockerfile to include Composer, because the project may have PHP dependencies not available in the stock Composer image.
 mkdir -p tmp/composer-cache
-winpty docker run --rm --interactive --tty --volume /$(pwd):/app --volume "${COMPOSER_HOME:-$HOME/.composer}:/tmp" composer install
+# shellcheck disable=SC2046
+winpty docker run --rm --interactive --tty \
+    --volume /$(pwd):/app \
+    --volume "${COMPOSER_HOME:-$HOME/.composer}:/tmp" \
+    composer install
+
+# TODO: Consider --environment TMPDIR=/tmp/composer to not have to mount over /tmp
